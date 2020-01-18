8d2894a1ebd9c538095483bf63da3b009b05a3d7
Gokul prabhu <gokul.prabhu@freshworks.com>
2020-01-19 01:01:05 +0530

    Issue fix ui components

--- src/utils/ui/confirm.ts
+++ src/utils/ui/confirm.ts
@@ -1,11 +1,12 @@
 import { prompt } from 'enquirer';
 
 export default async (message: string, initialValue = false): Promise<boolean> => {
-  const response = await prompt<boolean>({
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const response = await prompt<any>({
     type: 'confirm',
     name: 'value',
     message,
     initial: initialValue,
   });
-  return response;
+  return response.value;
 };
